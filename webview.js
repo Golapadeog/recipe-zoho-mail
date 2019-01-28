d19a961aa1ab38db1b3ad8ec0f0bd8ead7f49229
Jérémy LANDON <jeremy.landon@outlook.com>
2019-01-28 13:51:43 +0100

    show unread message count

--- webview.js
+++ webview.js
@@ -1,6 +1,17 @@
 module.exports = Franz => {
   const getMessages = function getMessages() {
     let count = 0;
+    const unreadCountElement = document.querySelectorAll(".unreadCount");
+
+    if (
+      unreadCountElement.length > 0 &&
+      unreadCountElement[0].innerText.length > 0
+    ) {
+      const countInElement = unreadCountElement[0].innerText.split(" ")[0];
+      if (!isNaN(countInElement)) {
+        count = +countInElement;
+      }
+    }
 
     // set Franz badge
     Franz.setBadge(count);
