e368bcabfd43ff79669cb610a7790a865b462445
Golapadeog <jeremy.landon@outlook.com>
2019-09-28 07:52:45 +0200

    update to new Zoho mail UI

--- webview.js
+++ webview.js
@@ -1,20 +1,7 @@
 module.exports = Franz => {
   const getMessages = function getMessages() {
-    let count = 0;
-    const unreadCountElement = document.querySelectorAll(".unreadCount");
-
-    if (
-      unreadCountElement.length > 0 &&
-      unreadCountElement[0].innerText.length > 0
-    ) {
-      const countInElement = unreadCountElement[0].innerText.split(" ")[0];
-      if (!isNaN(countInElement)) {
-        count = +countInElement;
-      }
-    }
-
     // set Franz badge
-    Franz.setBadge(count);
+    Franz.setBadge(document.querySelectorAll(".zmLUrd").length);
   };
 
   // check for new messages every second and update Franz badge
