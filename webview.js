24bfc353c6e4f415d2e31980854ca4ba7472e4e3
Golapadeog <jeremy.landon@outlook.com>
2019-10-22 16:44:38 +0200

    use "unread" section

--- webview.js
+++ webview.js
@@ -1,7 +1,11 @@
 module.exports = Franz => {
   const getMessages = function getMessages() {
+    var c = $(".jsZmTreeText:contains('Unread')")
+      .closest('.zmTreeNode')
+      .find('.zmTreeCount')
+      .text();
     // set Franz badge
-    Franz.setBadge(document.querySelectorAll(".zmLUrd").length);
+    Franz.setBadge(c.length == 0 ? 0 : c);
   };
 
   // check for new messages every second and update Franz badge
